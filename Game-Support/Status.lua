f5e966bdc67a7c8228115e52bf68d50f11ca971e
Phantomnism <123495952+123hoipopper13r6@users.noreply.github.com>
2026-03-29 16:54:32 +0500

    Add 'UNIVERSAL' status to Status.lua

--- Game-Support/Status.lua
+++ Game-Support/Status.lua
@@ -1,1 +1,1 @@
-{82638711520338 = "Working | Alpha"; 122764594952227 = "Working | Alpha"; 120331934510435 = "Working"}
+{82638711520338 = "Working | Alpha"; 122764594952227 = "Working | Alpha"; 120331934510435 = "Working"; UNIVERSAL = "In development";}
