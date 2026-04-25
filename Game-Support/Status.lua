1e3e2811df29f6b6d6a31d542bbbbd3dd9d181bd
Phantomnism <123495952+123hoipopper13r6@users.noreply.github.com>
2026-04-25 23:26:10 +0500

    Update status of item 82638711520338 to 'Broken | Alpha'

--- Game-Support/Status.lua
+++ Game-Support/Status.lua
@@ -1,1 +1,1 @@
-{82638711520338 = "Working | Alpha"; 122764594952227 = "Working | Beta"; 120331934510435 = "Working"; UNIVERSAL = "In development";}
+{82638711520338 = "Broken | Alpha"; 122764594952227 = "Working | Beta"; 120331934510435 = "Working"; UNIVERSAL = "In development";}
